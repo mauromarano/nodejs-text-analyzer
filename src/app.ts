e95d9f8e4c0d9b95c0bd9874371c3c557d3b991e
mauromarano <gagginaspinnata@gmail.com>
2024-05-14 15:26:03 +0000

    aggiunto fastify static

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+// Libreria
 import analyze from "./lib";
 
 // Import the framework and instantiate it
@@ -6,8 +7,19 @@ const fastify = Fastify({
   logger: true,
 });
 
+const path = require("path");
+const fastifyStatic = require("@fastify/static");
+
+// Registra il plugin fastify-static
+fastify.register(fastifyStatic, {
+  root: "/workspaces/nodejs-text-analyzer/public", // Percorso in cui si trovano i file statici
+  prefix: "/public/", // URL prefix per accedere ai file statici
+});
+
+
 // Declare a route
-fastify.get("/", async function handler(request, reply) {
+fastify.post("/api", async function handler(request, reply) {
+  console.log(request.body);
   return { hello: "world!!!!" };
 });
 
