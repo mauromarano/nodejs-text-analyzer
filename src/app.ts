739226d9e39026fb982aea927a6ed78f30bfde2a
mauromarano <gagginaspinnata@gmail.com>
2024-05-14 14:28:44 +0000

    aggiunto il server

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,23 @@
-import analyze   from './lib';
+import analyze from "./lib";
 
+// Import the framework and instantiate it
+import Fastify from "fastify";
+const fastify = Fastify({
+  logger: true,
+});
+
+// Declare a route
+fastify.get("/", async function handler(request, reply) {
+  return { hello: "world!!!!" };
+});
+
+async function main() {
+  // Run the server!
+  try {
+    await fastify.listen({ host: "0.0.0.0", port: 3000 });
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+}
+main();
