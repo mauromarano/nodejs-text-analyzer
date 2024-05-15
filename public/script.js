f738caa0566710e05b7d42f3bf38f280b8e30102
mauromarano <gagginaspinnata@gmail.com>
2024-05-15 13:45:36 +0000

    rimossi commenti inutili

--- public/script.js
+++ public/script.js
@@ -5,12 +5,11 @@ async function analyzeText() {
     '<div class="text-center"><svg class="animate-spin -ml-1 mr-3 h-5 w-5 text-blue-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"><circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle><path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 0116 0h2a10 10 0 00-20 0h2z"></path></svg>Caricamento...</div>';
 
   const response = await fetch("/api", {
-    method: "POST", // *GET, POST, PUT, DELETE, ecc.
-
+    method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify(input), // body data type must match "Content-Type" header
+    body: JSON.stringify(input),
   });
 
   let result = await response.json();
