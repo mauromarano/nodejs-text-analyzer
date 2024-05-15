a11ee0cb43d8894c76c0237e9ee285615d5929c4
mauromarano <gagginaspinnata@gmail.com>
2024-05-15 06:56:03 +0000

    aggiunti commenti

--- src/lib.ts
+++ src/lib.ts
@@ -1,8 +1,10 @@
 import fs from 'fs';
 import axios from 'axios';
 
-// const url:string = 'https://pastebin.com/raw/gvXmL4yU'
+// Indica quante volte una parola deve ripetersi 
+const doppioni: number = 10;
 
+// Funzione asincrona per leggere il contenuto di un file dato un percorso
 async function readFileContent(path: string): Promise<string> {
   if (path.startsWith('http://') || path.startsWith('https://')) {
     const response = await axios.get(path);
@@ -12,11 +14,13 @@ async function readFileContent(path: string): Promise<string> {
   }
 }
 
+// Funzione per analizzare il testo
 function analyzeText(text: string) {
     const wordCount = text.split(/\s+/).length;
     const letterCount = text.replace(/\s+/g, '').length;
     const spaceCount = (text.match(/\s/g) || []).length;
   
+    // Memorizza la frequenza delle parole
     const wordFrequency: { [key: string]: number } = {};
     const words = text.toLowerCase().match(/\b\w+\b/g) || [];
     words.forEach(word => {
@@ -27,19 +31,17 @@ function analyzeText(text: string) {
       }
     });
   
+    // Trova le parole ripetute più di doppioni volte
     const repeatedWords = Object.entries(wordFrequency)
-      .filter(([word, count]) => count > 10)
+      .filter(([word, count]) => count > doppioni)
       .map(([word, count]) => ({ word, count }));
   
     return { wordCount, letterCount, spaceCount, repeatedWords };
-  }
-  
+}
 
-
-async function analyze(url:string) {
-
-  const text:string = await readFileContent(url);
-
+// Funzione asincrona per analizzare il contenuto di un URL o di un file
+async function analyze(url: string) {
+  const text: string = await readFileContent(url);
   const result = analyzeText(text);
   return result;
 }
